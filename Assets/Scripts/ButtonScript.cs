983220467addfe3edb84fa4466f8984c5464f5d3
Valtteri Rasila <valtteri.rasila@student.tut.fi>
2018-10-25 23:08:40 +0300

    menu improvements, new (non-functional) combat mode scene

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void ChangeScene(int sceneNo) {
         SceneManager.LoadScene(sceneNo);
     }
