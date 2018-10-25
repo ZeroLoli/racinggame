83d9923a67bb347e34b03c3af6ade7eba1a07360
Valtteri Rasila <valtteri.rasila@student.tut.fi>
2018-10-25 16:05:09 +0300

    simple menu added

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeScene(int sceneNo) {
+        SceneManager.LoadScene(sceneNo);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
